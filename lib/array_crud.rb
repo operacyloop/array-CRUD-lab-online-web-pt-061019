c5210a3066d5529e2708427fb352bc2ca9d7c3bd
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-06-11 02:47:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,6 +22,9 @@ end
 
 def remove_element_from_end_of_array(array)
   
+  famous_cats = ["lil' bub", "grumpy cat", "Maru"]
+  maru_cat = famous_cats.pop
+  
 end
 
 def remove_element_from_start_of_array(array)
